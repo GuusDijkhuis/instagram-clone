e3616574252942e5671b7c78f840e0a655a8630f
Guus Dijkhuis <guus.dijkhuis@gmail.com>
2022-01-06 12:57:50 +0100

    convert firebase to v9

--- components/Main.js
+++ components/Main.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchUser } from '../redux/actions'
 
 export class Main extends Component {
 	componentDidMount() {
-
+		this.props.fetchUser();
 	}
 	render() {
 		return (
@@ -14,4 +18,13 @@ export class Main extends Component {
 	}
 }
 
-export default Main
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center'
+	}
+})
+
+const mapDispatchProps = (dispatch) => bindActionCreators({ fetchUser }, dispatch);
+
+export default connect(null, mapDispatchProps)(Main);
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,14 +1,16 @@
 import { USER_STATE_CHANGE } from '../constants';
-import { auth } from '@firebase/auth';
+import { getAuth } from '@firebase/auth';
 import { doc, getDoc } from "@firebase/firestore";
+import { db } from '../../firebaseConfig';
+
 
 export function fetchUser() {
 	return ((dispatch) => {
-		const colRef = doc(db, "users", auth().currentUser)
+		const colRef = doc(db, "users", getAuth().currentUser.uid)
 		getDoc(colRef)
 		.then(snapshot => {
 			if(snapshot.exists) {
-				dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data })
+				dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() })
 			} else {
 				console.log('User does not exist');
 			}
